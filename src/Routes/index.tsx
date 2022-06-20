fe9e8409fe45d75f3d0b00c41b3e509e374423a9
Olafi-MooN <alefmastertutor@gmail.com>
2022-06-19 21:13:23 -0300

    Add link in pages

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -6,15 +6,25 @@ import {
 import { Home } from "../Pages/Home";
 import { Quiz } from "../Pages/Quiz";
 import { LandingPage } from "../Pages/Landingpage";
+import { Profile } from "../Pages/Profile";
+import { ProfileEdit } from "../Pages/Profile/ProfileEdit";
+import { Navbar } from "../Components/Navbar";
 
 const Router = () => {
   return (
+
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/quiz/:id" element={<Quiz />}></Route>
       </Routes>
+      <Navbar>
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/quiz/:id" element={<Quiz />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/profile/edit" element={<ProfileEdit />}></Route>
+        </Routes>
+      </Navbar>
     </BrowserRouter>
   )
 }
